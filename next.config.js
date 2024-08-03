115ee921dfbea20244ee0c72cdc383e2f248bcf8
John O'Brien <62157096+johnobrien8642@users.noreply.github.com>
2024-08-02 21:17:49 -0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+			{
+				protocol: "http",
+				hostname: "**",
+			},
+		],
+  	},
+	typescript: {
+		ignoreBuildErrors: true,
+	}
+};
